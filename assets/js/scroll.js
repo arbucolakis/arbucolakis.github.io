03a114e805559a2503d62395a0487f4c8977c3cb
adarsh patil <patiladarsh98032321212@gmail.com>
2024-10-02 00:15:33 +0530

    other updates

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -11,7 +11,9 @@ $(document).ready(function(){
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 700, function(){
-                window.location.hash = hash;
+                //window.location.hash = hash;
+                // Instead of updating window.location.hash, use history.pushState
+                window.history.pushState(null, null, '/' + hash.substring(1)); // Update the URL to /section
             });
         } 
     });
